a259f25300959912ce1b8530404b766293a69fb0
pankajraj-tse <pankajraj25dec@gmail.com>
2025-03-27 18:13:27 +0100

    this is last commit

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  description = "This is the AWS provider"
+
 }
 
 # Create a VPC
-resource "aws_vpc" "exmaple_vc" {
+resource "aws_vpc" "exmapl_vc" {
   cidr_block = "10.0.0.0/16"
 
 }
